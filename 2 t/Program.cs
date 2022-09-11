abc850a19ed21258ede6748de027dca01976ee9b
KET <varlamova_e.t@mail.ru>
2022-09-11 18:33:53 +0300

    2t

---
+++ 2 t/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Summa(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        sum = sum + x%10;
+        x = x/10;
+    }
+    return $"Сумма цифр в числе равна {sum}";
+}
+string result = Summa(n);
+Console.WriteLine(result);
